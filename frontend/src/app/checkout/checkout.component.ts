c39410946739f060bc4dc9cffa70f6f8932ea23b
ntphi <phinguyen202@gmail.com>
2019-05-14 00:19:31 +0700

    checkout page: bill detail

--- frontend/src/app/checkout/checkout.component.ts
+++ frontend/src/app/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/service/cart.service';
+import { ProductService } from 'src/service/product.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  products = [];
+  sumOfAmount:number = 0;
+  constructor(private productService: ProductService,
+    private cartService: CartService) {
+    this.products = this.productService.getNewProducts();
+      this.products.forEach(product => {
+        product['buyingQuantity'] = 1;
+        product['amount'] = product['buyingQuantity'] * product['price'];
+        this.sumOfAmount += product['amount'];
+      });
+   }
 
   ngOnInit() {
   }
